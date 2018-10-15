d7afbca635e194c57bf828f24544a2866c8b4c2d
mchiang88 <merlinchiang@gmail.com>
2018-10-15 14:50:38 -0700

    rendered login and signup pages, added table but not sure if it's created

--- app/config.js
+++ app/config.js
@@ -39,6 +39,16 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
-
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (click) {
+      click.increments('id').primary();
+      click.string('username', 100);
+      click.string('password', 100);
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
 module.exports = db;
